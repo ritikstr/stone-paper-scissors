ccfab55e0e7c49484c5841fdae8c1f4a4624e5fd
ritikstr <ritikrana5949@gmail.com>
2024-04-03 10:08:11 +0530

    logic

---
+++ app.js
@@ -0,0 +1,55 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+    //rock, paper, scissors
+}
+
+
+const drawGame = () => {
+    console.log("game was draw!");
+}
+
+const showWinner = (userWin) => {
+    if(userWin) {
+        console.log("you win!");
+    } else {
+        console.log("you lose!");
+    }
+}
+
+const playGame = (userChoice) => {
+    console.log("userChoice = ", userChoice);
+    //Generate the comp choice 
+    const compChoice = genCompChoice();
+    console.log("comp Choice = ", compChoice);
+
+    if(userChoice === compChoice) {
+        //Draw Game
+        drawGame();
+    } else {
+        let userWin = true;
+        if(userChoice === "rock") {
+            userWin = compChoice === "paper" ? false : true;
+        } else if(userChoice === "paper") {
+            //rock, scissors
+            userWin = compChoice === "scissors" ? false : true;
+        } else {
+            //rock, paper
+            userWin = compChoice === "rock" ? false : true; 
+        }
+      showWinner(userWin); 
+    }
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
